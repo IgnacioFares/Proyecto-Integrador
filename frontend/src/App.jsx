d75637631048eb0da36c62dc49e331c0d7168626
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-05-13 20:54:41 -0300

    actualizado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from './Components/Footer/Footer';
 import Calificacion from './Components/Calificacion/Calificacion'
+import Preguntas from './Preguntas/Preguntas';
 
 function App() {
   
 
   return (
     <div>
+        <Preguntas />
         <Calificacion />
         <Footer />
     </div>
