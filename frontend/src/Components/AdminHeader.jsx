ef55dce923e0e1787daa2eb74b12aba236955236
Fernando Mercy <nando.mercy@gmail.com>
2024-05-19 13:06:29 -0400

    se crea funcionalidad de boton agregar productos y eliminar productos, falta validacion de duplicados y poder subir 5 imagenes

--- frontend/src/Components/AdminHeader.jsx
+++ frontend/src/Components/AdminHeader.jsx
@@ -3,7 +3,11 @@
 const AdminHeader = ({ onOpenAddProductModal }) => {
   return (
     <header className="bg-white shadow p-4 flex justify-between items-center">
-      <h1 className="text-2xl font-semibold">VisualStudio Service</h1>
+      <input
+        type="text"
+        placeholder="Buscar productos..."
+        className="border p-2 rounded w-1/3"
+      />
       <button
         onClick={onOpenAddProductModal}
         className="px-4 py-2 bg-blue-500 text-white rounded"
