3a5316c854e4d071ddc414a5bf9e2777e44f3185
Ignacio Fares <ignaciofares7@gmail.com>
2024-05-20 20:16:33 -0300

    Se agregaron cambion el el header con respecto a las rutas de los logos

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="fixed top-0 left-0 w-full shadow-md h-16 bg-white text-green-500 flex items-center justify-between z-10">
       <div className="flex items-center">
         <Link to={routes.home}>
-          <img src={""} alt="Logo" className="h-8 w-150 m-4" />
+          <img src={"../../public/images/logosolo1.png"} alt="Logo" className="h-8 w-150 m-4" />
         </Link>
       </div>
 
