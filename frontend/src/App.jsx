5d7204a33b15d3a728db1cd0757f4161257be8fa
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-05-27 18:28:31 -0300

    actualizado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,15 @@ import Footer from './Components/Footer/Footer';
 import Calificacion from './Components/Calificacion/Calificacion'
 import Preguntas from './Preguntas/Preguntas';
 import Reservas from './Components/Reservas/Reservas';
-import FormularioRegistro from './Components/FormularioRegistro/FormularioRegistro';
+import FormularioRegistroAdmin from './Components/FormularioRegistroAdmin/FormularioRegistroAdmin';
+
 
 function App() {
   
 
   return (
     <div>
-        <FormularioRegistro/>
+        <FormularioRegistroAdmin/>
         <Footer/>
     </div>
   )
