4f6e4b92228e802e3c36283bba64bbb329aeaf77
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-06-06 17:55:56 -0300

    actualizado

--- Backend/src/main/java/com/easyscore/repository/ProductoRepository.java
+++ Backend/src/main/java/com/easyscore/repository/ProductoRepository.java
@@ -4,6 +4,7 @@ import com.easyscore.model.Producto;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -13,4 +14,12 @@ public interface ProductoRepository extends JpaRepository<Producto, Long> {
     @EntityGraph(attributePaths = {"caracteristicas"})
     @Query(value = "SELECT * FROM Producto ORDER BY RAND()", nativeQuery = true)
     List<Producto> findAllRandom();
+
+    @Query("SELECT p FROM Producto p WHERE LOWER(p.nombre) LIKE :searchTerm AND (:category IS NULL OR p.categoria.nombre = :category) AND (:startDate IS NULL OR p.horarioApertura >= :startDate) AND (:endDate IS NULL OR p.horarioCierre <= :endDate)")
+    List<Producto> findByNombreLikeAndCategoria(
+            @Param("searchTerm") String searchTerm,
+            @Param("category") String category,
+            @Param("startDate") String startDate,
+            @Param("endDate") String endDate
+    );
 }
