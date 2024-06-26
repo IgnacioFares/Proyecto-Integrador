c21e0a506770c4c77b592d99d181861faad3080e
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-06-26 15:25:46 -0300

    ajustando detalles

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = () => {
         <img src="/images/logosolo1.png" alt="Logo" className="mb-4 md:mb-0" />
        
         <div className="flex md:flex-row">
-            <a href="#" className="px-4"><FontAwesomeIcon icon={faFacebookF} /></a>
+            <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A//easyscore-beta.vercel.app/" className="px-4"><FontAwesomeIcon icon={faFacebookF} /></a>
             <a href="#" className="px-4"><FontAwesomeIcon icon={faInstagram} /></a>
-            <a href="#" className="px-4"><FontAwesomeIcon icon={faTwitter} /></a>
-            <a href="#" className="px-4"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+            <a href="https://twitter.com/intent/tweet?text=Encuentren%20las%20mejores%20canchas%20de%20futbol%20en%20https%3A//easyscore-beta.vercel.app/" className="px-4"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href="https://www.linkedin.com/shareArticle?mini=true&url=https%3A//easyscore-beta.vercel.app/" className="px-4"><FontAwesomeIcon icon={faLinkedinIn} /></a>
             <a href="#" className="px-4"><FontAwesomeIcon icon={faYoutube} /></a>
         </div>
     </div>
